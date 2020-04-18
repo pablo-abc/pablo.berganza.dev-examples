bcfe0a04bb6e3be50287bbe3d986e8fa4094d67f
Pablo Berganza <pberganza10@gmail.com>
2020-04-18 04:53:15 -0600

    :sparkles: Add svelte todo app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Examples for pablo.berganza.dev
+
+This just contains examples to be embedded in pablo.berganza.dev
---
+++ svelte-basic-todo-app/src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+  let todos = [], value = '';
+  let filter = 'all';
+  $: filtered = todos.filter(todo => {
+    if (filter === 'checked') return todo.checked
+    if (filter === 'unchecked') return !todo.checked
+    return todo
+  })
+  function addTodo() {
+    todos = [...todos, { value, id: Date.now(), checked: false }];
+    value = '';
+  }
+  function toggleTodo(event) {
+    const i = todos.findIndex(todo => todo.id === +event.target.id)
+    todos[i].checked = event.target.checked
+  }
+</script>
+
+<form>
+  <label for=all>
+    <input type=radio id=all value=all bind:group={filter}>
+    All
+  </label>
+  <label for=checked>
+    <input type=radio id=checked value=checked bind:group={filter}>
+    Checked
+  </label>
+  <label for=unchecked>
+    <input type=radio id=unchecked value=unchecked bind:group={filter}>
+    Unchecked
+  </label>
+</form>
+<form on:submit|preventDefault={addTodo}>
+  <input bind:value>
+  <button type=submit>Add Todo</button><br>
+</form>
+<ul>
+  {#each filtered as todo, i (todo.id)}
+    <li>
+      <input
+        id={todo.id}
+        checked={todo.checked}
+        type="checkbox"
+        on:change={toggleTodo}
+        >
+      {todo.value}
+    </li>
+  {/each}
+</ul>
+
+<style>
+  label {
+      display: inline-block;
+      margin: 0 10px;
+  }
+  li {
+      list-style: none;
+  }
+</style>
