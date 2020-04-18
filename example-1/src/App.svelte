2960ae1d9f2ab4c3b3ec590e24e9e8a1d351361b
Pablo Berganza <pberganza10@gmail.com>
2020-04-18 02:44:46 -0600

    :tada: Initial commit

---
+++ example-1/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  let message = 'Hello world!'
+
+  function onInput(event) {
+    message = event.target.value
+  }
+</script>
+
+<p>{message}</p>
+
+<input value={message} on:input={onInput}>
